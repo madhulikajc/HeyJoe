b7daf3c035e705ff9c1d9a00d4d99a887ef4040d
MJC <madhulikajain@gmail.com>
2020-03-17 17:34:32 -0700

    Added a second hand labeled real world training data set
    This data set has not been used in any models #18 or before, and
    could be used to develop/test them. Note in future commits
    if a model is trained using this data, this would mean it could
    not be used in testing those models.

--- label_save_dev_set2.py
+++ label_save_dev_set2.py
@@ -13,6 +13,7 @@ from heyjoe_helper_functions import *
 ## This code saves the hand labeled sets as X_hand_label_train2.npy and Y..
 
 WAV_PATH = "/Users/mjain/Desktop/HeyJoe_data/raw_data_wav/dev_set/001/"
+TRAINING_PATH = "/Users/mjain/Desktop/HeyJoe_data/Training_Data/"
 
 num_examples = 25 #10 with Hey joe activates and 15 without
 Ty = 1375
@@ -33,48 +34,47 @@ Y[0] = (insert_ones(Y[0].T, (5511-50)/5511 * ten_seconds)).T
 filename = WAV_PATH + "19.wav"
 x = graph_spectrogram(filename)
 X[1] = x.T
-Y[1] = (insert_ones(Y[1].T, 1420/5511 * ten_seconds)).T
+Y[1] = (insert_ones(Y[1].T, 1950/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "20.wav"
 x = graph_spectrogram(filename)
 X[2] = x.T
-Y[2] = (insert_ones(Y[2].T, 1600/5511 * ten_seconds)).T
+Y[2] = (insert_ones(Y[2].T, 2820/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "21.wav"
 x = graph_spectrogram(filename)
 X[3] = x.T
-Y[3] = (insert_ones(Y[3].T, 1830/5511 * ten_seconds)).T
+Y[3] = (insert_ones(Y[3].T, 2290/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "22.wav"
 x = graph_spectrogram(filename)
 X[4] = x.T
-Y[4] = (insert_ones(Y[4].T, 56/5511 * ten_seconds)).T
-Y[4] = (insert_ones(Y[4].T, 2150/5511 * ten_seconds)).T
+Y[4] = (insert_ones(Y[4].T, 4360/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "23.wav"
 x = graph_spectrogram(filename)
 X[5] = x.T
-Y[5] = (insert_ones(Y[5].T, 3200/5511 * ten_seconds)).T
+Y[5] = (insert_ones(Y[5].T, 4800/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "24.wav"
 x = graph_spectrogram(filename)
 X[6] = x.T
-Y[6] = (insert_ones(Y[6].T, 4230/5511 * ten_seconds)).T
+Y[6] = (insert_ones(Y[6].T, 2950/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "25.wav"
 x = graph_spectrogram(filename)
 X[7] = x.T
-Y[7] = (insert_ones(Y[7].T, 3890/5511 * ten_seconds)).T
+Y[7] = (insert_ones(Y[7].T, 3350/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "26.wav"
 x = graph_spectrogram(filename)
 X[8] = x.T
-Y[8] = (insert_ones(Y[8].T, 3770/5511 * ten_seconds)).T
+Y[8] = (insert_ones(Y[8].T, 4150/5511 * ten_seconds)).T
 
 filename = WAV_PATH + "27.wav"
 x = graph_spectrogram(filename)
 X[9] = x.T
-Y[9] = (insert_ones(Y[9].T, 990/5511 * ten_seconds)).T
+Y[9] = (insert_ones(Y[9].T, 475/5511 * ten_seconds)).T
 
 
 
@@ -93,8 +93,8 @@ for i in range(31, 46):
 
 
 
-np.save('X_hand_label_train2', X)
-np.save('Y_hand_label_train2', Y)
+np.save(TRAINING_PATH + 'X_hand_label_train2', X)
+np.save(TRAINING_PATH + 'Y_hand_label_train2', Y)
 
 
 
